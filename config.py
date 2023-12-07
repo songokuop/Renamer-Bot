78fc1918941b03b19429f053bf86bdff3b68cb44
Krishna Singh <152683218+KrishnaTG@users.noreply.github.com>
2023-12-07 16:53:12 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,36 @@
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+
+import re, os
+
+id_pattern = re.compile(r'^.\d+$') 
+
+API_ID = os.environ.get("API_ID", "")
+
+API_HASH = os.environ.get("API_HASH", "")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+
+FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
+
+             # Don't Remove Credit @VJ_Botz
+             # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+             # Ask Doubt on telegram @KingVJ01
+
+DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
+
+DB_URL = os.environ.get("DB_URL", "")
+ 
+FLOOD = int(os.environ.get("FLOOD", "10"))
+
+START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
+
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5606411877').split()]
+
+PORT = os.environ.get("PORT", "8080")
+
+# Don't Remove Credit @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
